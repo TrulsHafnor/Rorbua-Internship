920e5675bb163fd0700c991fc8ed2e31c4dbf250
TrulsHafnor <Truls.Hafnor@outlook.com>
2022-02-02 17:30:54 +0100

    1. Lagt til en basic navbar 2. Hvis man ikke er logget inn har man ikke tilgang til å lage, redigere og slette stories. 3. Andre brukere som er logget inn kan ikke slette andre brukere sine stories. 4. Har linket stories og brukere, med migration 5. Det skal være mulig å gjette f.eks story id eller bruker id for å edite i url.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,4 @@
 class Story < ApplicationRecord
   has_one_attached :story_file
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_112640) do
+ActiveRecord::Schema.define(version: 2022_02_02_134044) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2022_02_02_112640) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_stories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
