0dfe854fde86fd3c6bede16f8631e6f672f4d907
Trygve <trygvejohannessen88@gmail.com>
2022-02-03 01:11:35 +0100

    Feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :stories
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "stories#index"
+  root "feed#index"
 end
