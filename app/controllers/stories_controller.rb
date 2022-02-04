7b9a0ca29f5d2fa7cc85db0c77694fe65d4dd0fb
TrulsHafnor <58667703+TrulsHafnor@users.noreply.github.com>
2022-02-04 14:42:53 +0100

    Update stories_controller.rb
    Endret slit at man kan trykke seg inn på story, selvom man ikke er logget inn

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,6 @@
 class StoriesController < ApplicationController
   before_action :set_story, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :correct_user, only: [:edit, :update, :destroy]
   # GET /stories or /stories.json
   def index
