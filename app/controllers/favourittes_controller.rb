6a6f8e6c1e32aecb07eeca9cfa2416d1ecfead73
Trygve <trygvejohannessen88@gmail.com>
2022-02-04 03:13:12 +0100

    fixet favorittes_controller

--- app/controllers/favourittes_controller.rb
+++ app/controllers/favourittes_controller.rb
@@ -24,31 +24,7 @@ class FavourittesController < ApplicationController
 
   end
 
-  # DELETE /stories/1 or /stories/1.json
-  def destroy
-    @story.destroy
-
-    respond_to do |format|
-      format.html { redirect_to stories_url, notice: "Story was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  def correct_user
-    @story = current_user.stories.find_by(id: params[:id])
-    redirect_to stories_path, notice: "Not Authorized To Edit This Story" if @story.nil?
-  end
-
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_story
-    @story = Story.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def story_params
-    params.require(:story).permit(:title, :description, :story_file, :user_id)
-  end
 
   def story_allready_added_to_favourittes
     logger.error "Attempt to add story already added to favorites"
